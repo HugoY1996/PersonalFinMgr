c9f3d94ebc91e0b235c22080896526311eb6277b
hugoyang1996@qq.com <Greg@yang2>
2020-09-11 10:48:35 +0800

    init

---
+++ src/main/java/com/hackathon/financemgr/FinancemgrApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.financemgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancemgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancemgrApplication.class, args);
+    }
+
+}
